d4801eeb635e92517484f3f3f2d7c09207870d50
liamflynn2003 <20098690@mail.wit.ie>
2023-12-12 15:30:44 +0000

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -7,9 +8,29 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
 const userValidator = (user) => {
     return user!==null;
 }
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
 
 const passwordValidator = (password) => {
     if(password==null){return false}

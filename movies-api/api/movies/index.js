7bfe4f50715e97a3c773fdd3031a7865a3e69b09
liamflynn2003 <20098690@mail.wit.ie>
2023-12-12 15:24:17 +0000

    Upcoming movies from TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,7 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
+import {getUpcomingMovies} from '../tmdb-api';
 
 const router = express.Router();
 
@@ -9,6 +10,10 @@ router.get('/', asyncHandler(async (req, res) => {
     res.status(200).json(movies);
 }));
 
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
 // Get movie details
 router.get('/:id', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
